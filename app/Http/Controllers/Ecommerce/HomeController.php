677064236bd2ae13795854aaf045bf5562ad3995
CamiloEscar <camiloescar1995@gmail.com>
2025-04-03 18:09:44 -0300

    Add: categoria para el front

--- app/Http/Controllers/Ecommerce/HomeController.php
+++ app/Http/Controllers/Ecommerce/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Ecommerce;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product\Categorie;
 use App\Models\Slider;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,8 @@ class HomeController extends Controller
     public function home(Request $request)
     {
         $sliders_principal = Slider::where("state", 1)->where("type_slider", 1)->orderBy("id", "desc")->get();
+
+        $categories_randoms = Categorie::where("categorie_second_id", NULL)->where("categorie_third_id", NULL)->orderBy("id", "desc")->get();
 
         return response()->json([
             "sliders_principal" => $sliders_principal->map(function ($slider) {
@@ -28,6 +31,13 @@ class HomeController extends Controller
                     "type_slider" => $slider->type_slider,
                     "price_original" => $slider->price_original,
                     "price_campaing" => $slider->price_campaing
+                ];
+            }),
+            "categories_randoms" => $categories_randoms->map(function ($categorie) {
+                return [
+                    "id" => $categorie->id,
+                    "name" => $categorie->name,
+                    "imagen" => env("APP_URL") . "storage/" . $categorie->imagen,
                 ];
             })
         ]);
