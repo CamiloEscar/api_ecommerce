12958834aff79b2c1b1af11144732853b2f87952
CamiloEscar <camiloescar1995@gmail.com>
2025-04-09 22:31:16 -0300

    Modelado de la BD, creacion de archivos de controladores, collection y resource y modelo

---
+++ app/Models/Sale/Cart.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models\Sale;
+
+use App\Models\Product\Product;
+use App\Models\Product\ProductVariation;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        "user_id",
+        "product_id",
+        "type_discount",
+        "discount",
+        "type_campaing",
+        "code_cupon",
+        "code_discount",
+        "product_variation_id",
+        "quantity",
+        "price_unit",
+        "subtotal",
+        "total",
+        "currency"
+    ];
+
+    public function setCreatedAtAttribute($value)
+    {
+        date_default_timezone_set("America/Argentina/Buenos_Aires");
+        $this->attributes["created_at"] = Carbon::now();
+    }
+    public function setUpdatedAtAttribute($value)
+    {
+        date_default_timezone_set("America/Argentina/Buenos_Aires");
+        $this->attributes["updated_at"] = Carbon::now();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+    public function product_variation()
+    {
+        return $this->belongsTo(ProductVariation::class);
+    }
+}
